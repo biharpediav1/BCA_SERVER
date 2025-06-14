d82d167792392ceea8638a6afdc454410a7de46b
vishalkatsa <papajikatsa@gmail.com>
2025-06-14 06:35:09 +0530

    vercel2

--- src/api/hello.ts
+++ src/api/hello.ts
@@ -1,5 +1,5 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+// import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ message: "Hello from Vercel serverless function!" });
-}
+// export default function handler(req: VercelRequest, res: VercelResponse) {
+//   res.status(200).json({ message: "Hello from Vercel serverless function!" });
+// }
